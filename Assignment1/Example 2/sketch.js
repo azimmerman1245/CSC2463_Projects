758a49bd48f6092777f0a9e76ae7a12dd7f76fff
Aalok Zimmerman <azimmerman1245@gmail.com>
2024-01-30 20:03:34 -0600

    Example 2

---
+++ Assignment1/Example 2/sketch.js
@@ -0,0 +1,14 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+  noStroke();
+  fill(255, 0, 0, 75)
+  circle(200, 150, 100);
+  fill(51, 51, 255, 75)
+  circle(175, 200, 100);
+  fill(51, 255, 51, 75)
+  circle(225, 200, 100);
+}
