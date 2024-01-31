2f1280aa151126f762688e51ff46edb91076009a
Aalok Zimmerman <azimmerman1245@gmail.com>
2024-01-30 20:53:37 -0600

    Adjust alpha levels and background colors

--- Assignment1/Example 2/sketch.js
+++ Assignment1/Example 2/sketch.js
@@ -3,12 +3,12 @@ function setup() {
 }
 
 function draw() {
-  background(220);
+  background(255, 255, 255);
   noStroke();
-  fill(255, 0, 0, 75)
+  fill(255, 0, 0, 80)
   circle(200, 150, 100);
-  fill(51, 51, 255, 75)
+  fill(51, 51, 255, 80)
   circle(175, 200, 100);
-  fill(51, 255, 51, 75)
+  fill(51, 255, 51, 80)
   circle(225, 200, 100);
 }
